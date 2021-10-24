905b2d0faa93c93707248da82efaa6ec4e895645
sunny <sunny.ghosh26@gmail.com>
2021-10-24 16:09:15 +0100

    added Dockerfile

---
+++ src/main/java/com/joy_feebles/jenkinspro/JenkinsProApplication.java
@@ -0,0 +1,14 @@
+package com.joy_feebles.jenkinspro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsProApplication {
+
+	public static void main(String[] args) {
+		System.out.println(" ******* jenkins pro application start ******");
+		SpringApplication.run(JenkinsProApplication.class, args);
+	}
+
+}
